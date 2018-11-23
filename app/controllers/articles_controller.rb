590a56bbb163a0024ebab19971540a729d16ac72
Rails Udemy Course <railsudemycourse@gmail.com>
2018-11-23 14:52:13 +0200

    Erros styling added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
     
         if @article.save
     
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
     
             redirect_to article_path(@article)
     
@@ -40,7 +40,7 @@ class ArticlesController < ApplicationController
     
         if @article.update(article_params)
     
-            flash[:notice] = "Article was successfully updated"
+            flash[:updated] = "Article was successfully updated"
     
             redirect_to article_path(@article)
     
@@ -60,7 +60,7 @@ class ArticlesController < ApplicationController
     
         @article.destroy
     
-        flash[:notice] = "Article was successfully deleted"
+        flash[:danger] = "Article was successfully deleted"
     
         redirect_to articles_path
     
