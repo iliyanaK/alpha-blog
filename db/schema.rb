fe7635970df5c64f80bf3de9860d63a4578aeb54
Rails Udemy Course <railsudemycourse@gmail.com>
2018-11-26 17:10:11 +0200

    Set secure password for a user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_120601) do
+ActiveRecord::Schema.define(version: 2018_11_26_150346) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_120601) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
